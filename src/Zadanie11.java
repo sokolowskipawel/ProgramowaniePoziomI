07d12edad136bcdb74e78377efe599636a1c0ee2
pawelsokolowski <p.pawel.sokolowski@gmail.com>
2018-06-26 22:48:36 +0200

    Zadanie 11 działające

--- src/Zadanie11.java
+++ src/Zadanie11.java
@@ -4,41 +4,38 @@ public class Zadanie11 {
     private String a;
     private String commonText;
 
-    public String findCommonText(String text1, String text2){
+    public String findCommonText(String text1, String text2) {
 
         String textCommon = text2;
 
-        if (text1.length() < text2.length()){
+        if (text1.length() < text2.length()) {
             text2 = text1;
             text1 = textCommon;
             textCommon = text2;
         }
-
-        int beginIndex = 0;
-        int endIndex = text2.length();
-
-        while (endIndex <= text1.length()){
-            if (isTheSameTexts(text1.substring(beginIndex, endIndex), textCommon)){
-                return textCommon;
+        while (textCommon.length() > 0) {
+            if (text1.contains(textCommon)) {
+                commonText = textCommon;
+                return commonText;
             }
-            beginIndex ++;
-            endIndex ++;
+            if (text1.contains(textCommon.substring(1))) {
+                commonText = textCommon.substring(1);
+                return commonText;
+            }
+            if (text1.contains(textCommon.substring(0, textCommon.length() - 1))) {
+                commonText = textCommon.substring(0, textCommon.length() - 1);
+                return commonText;
+            }
+            textCommon = textCommon.substring(1, textCommon.length() - 1);
         }
-
-        return null;
+        return commonText;
     }
 
-    boolean isTheSameTexts(String text1, String text2){
-        if (text1.equals(text2)){
-            return true;
-        }
-        return false;
-    }
     public static void main(String[] args) {
 
         Zadanie11 zadanie11 = new Zadanie11();
 
-        System.out.println(zadanie11.findCommonText("AAABBA", "ABAAABBAAA"));
+        System.out.println(zadanie11.findCommonText("AAABBA", "ABAABBAAA"));
 
     }
 }
